cb9dddee6670e84e3fca23e3ffe5e4a9d5e1086d
Philip Dayboch <philip245@gmail.com>
2024-07-13 11:26:44 -0400

    Fix token refresh
    Fixes the token refresh logic to correctly request a new access token
    when the current one expires.

--- lib/drive_api.rb
+++ lib/drive_api.rb
@@ -119,12 +119,14 @@ class DriveApi
     )
     user_id = "default"
     credentials = authorizer.get_credentials(user_id)
-    if credentials.nil? || credentials.needs_access_token?
+    if credentials.nil?
       url = authorizer.get_authorization_url(base_url: OOB_URI)
-      puts "TOKEN REFRESH NEEDED.\n"
       puts "Open the following URL in the browser and enter the resulting code after authorization:\n\n" + url
       code = gets
       credentials = authorizer.get_and_store_credentials_from_code(user_id: user_id, code: code, base_url: OOB_URI)
+    elsif credentials.expired?
+      puts("refreshing token...")
+      credentials.refresh!
     end
 
     credentials
